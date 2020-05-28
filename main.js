804e964f3054c9aec933856e8765dba9da278b61
KewenSilva <kewen.macedo@gmail.com>
2020-05-28 19:03:35 -0300

    Classes

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-alert('teste');
+class List {
+    constructor() {
+        this.data = [];
 
-class Teste{
-    metodo(){
+    }
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+
+    }
+}
+
+class TodoList extends List {
+
+    constructor() {
+        super();
+
+        this.usuario = "Kewen";
+    }
+    mostrarUsuario(){
+        console.log(this.usuario);
         
     }
 }
+var MinhaLista = new TodoList();
+document.getElementById('novoTodo').onclick = function () {
+    MinhaLista.add('Novo todo');
+}
+MinhaLista.mostrarUsuario();
